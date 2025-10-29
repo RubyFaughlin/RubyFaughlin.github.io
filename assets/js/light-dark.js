69e96c7c48e0f7b07024833735c0c1978a179dc3
RubyFaughlin <S00345694@MYACU.EDU.AU>
2025-10-29 21:31:08 +1100

    Add files via upload

--- assets/js/light-dark.js
+++ assets/js/light-dark.js
@@ -1,19 +1,23 @@
-// --- JavaScript Feature for Switching between Light and Dark Mode ---
+/* JavaScript Feature for Light/Dark Mode */
+const btn = document.getElementById('theme-toggle');
+  const body = document.body;
 
-<button id="theme-toggle">Switch to Dark Mode</button>
-<script>
- const btn = document.getElementById('theme-toggle');
- const body = document.body;
+  btn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode'); /* switch dark mode on/off */
+
+    const textElements = document.querySelectorAll(
+      '#main h1, #main h2, #main h3, #main h4, #main h5, #main h6, #main p, #main a, #main strong, #main .button.large, theme-toggle'
+    );
+
+    if (body.classList.contains('dark-mode')) {
+      btn.textContent = "Switch to Light Mode";
+      textElements.forEach(el => el.style.color = '#fff'); /* white text */
+	  
+    } else {
+      btn.textContent = "Switch to Dark Mode";
+      textElements.forEach(el => el.style.color = ''); /* reset to default (white mode) */
+	  
+    }
+  });
 
 
- btn.addEventListener('click', () => {
-   body.classList.toggle('dark');
-
-
-   if (body.classList.contains('dark')) {
-   btn.textContent = "Switch to Light Mode";
- } else {
-   btn.textContent = "Switch to Dark Mode";
- }
-});
-</script>
